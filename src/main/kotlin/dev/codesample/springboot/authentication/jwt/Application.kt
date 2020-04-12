3411c02c3749a3275a7c4046d8627fb6cb4bdb1e
Brian DiCasa <bdicasa@gmail.com>
2020-04-12 12:51:55 -0400

    Remove Configuration annotation from SpringBootApplication class as it is not required.

--- src/main/kotlin/dev/codesample/springboot/authentication/jwt/Application.kt
+++ src/main/kotlin/dev/codesample/springboot/authentication/jwt/Application.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-@Configuration
 class Application {
 
     @Bean
